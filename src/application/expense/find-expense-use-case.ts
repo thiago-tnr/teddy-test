b4871f3ba25f259a7d4956b90d7aa89b8fb92818
thiago-tnr <tnr.rocha@gmail.com>
2024-02-26 10:07:16 +0000

    feat: implements find  expense use case and tests

---
+++ src/application/expense/find-expense-use-case.ts
@@ -0,0 +1,36 @@
+import { type Expense } from '../../domain/entities/expense.entity'
+import { type Repository } from '../../infra/protocols/repository-interface'
+import { type UseCase } from '../../shared/application/protocol/use-case-interface'
+import { NotFoundError } from '../../shared/erros/not-found-error.er'
+
+export type FindExpenseInput = {
+  expense_id: string
+}
+// TODO updated_at is missing!
+export type FindExpenseOutPut = {
+  expense_id: string
+  description?: string
+  data?: Date
+  user_owner?: any
+  value?: number
+}
+
+export class FindExpenseUseCase implements UseCase<FindExpenseInput, FindExpenseOutPut> {
+  constructor (
+    private readonly repository: Repository<Expense>
+  ) {}
+
+  async execute (input: FindExpenseInput): Promise<FindExpenseOutPut> {
+    const expense = await this.repository.find(input.expense_id)
+
+    if (!expense) throw new NotFoundError()
+
+    return {
+      expense_id: expense.expense_id.id,
+      description: expense.description,
+      data: expense.data,
+      user_owner: expense.user_owner,
+      value: expense.value
+    }
+  }
+}
---
+++ src/tests/application/expense/test-find-expense-use-case.spec.ts
@@ -0,0 +1,44 @@
+import { FindExpenseUseCase } from '../../../application/expense/find-expense-use-case'
+import { Expense } from '../../../domain/entities/expense.entity'
+import { type Repository } from '../../../infra/protocols/repository-interface'
+import { NotFoundError } from '../../../shared/erros/not-found-error.er'
+
+describe('Find Expense UseCase', () => {
+  let useCase: FindExpenseUseCase
+  let expense: Expense
+  const repositoryMock: Repository<Expense> = {
+    create: jest.fn(),
+    update: jest.fn(),
+    find: jest.fn(),
+    delete: jest.fn()
+  }
+
+  beforeEach(async () => {
+    useCase = new FindExpenseUseCase(repositoryMock)
+  })
+
+  it('should be throw if expense not found', async () => {
+    try {
+      await useCase.execute({
+        expense_id: 'expense_id'
+      })
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundError)
+    }
+  })
+
+  it('should be able to update an expense', async () => {
+    expense = Expense.create({
+      description: 'create_any_description',
+      user_owner: 'create_any_user',
+      value: 123.45
+    })
+    await repositoryMock.create(expense)
+
+    jest.spyOn(repositoryMock, 'find').mockImplementation(() => Promise.resolve(expense))
+    const output = await useCase.execute({
+      expense_id: expense.expense_id.id
+    })
+    expect(output.expense_id).toEqual(expense.expense_id.id)
+  })
+})
